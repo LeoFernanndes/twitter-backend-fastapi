da5d9b4b06b90a5dd4160afa33895fd8d1a83faf
Ubuntu <ubuntu@ip-172-31-11-141.ec2.internal>
2022-01-31 02:54:17 +0000

    api container set up

--- services/files_handler.py
+++ services/files_handler.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 import pandas as pd
 
 from tweets.dto import TweetDto
@@ -6,4 +8,10 @@ from typing import List
 def save_local_user_tweets_csv(user_screen_name: str, tweets_list: List[TweetDto]) -> None:
     parsed_list = [tweet.__dict__ for tweet in tweets_list]
     dataframe = pd.DataFrame(parsed_list)
-    dataframe.to_csv(f'/home/ubuntu/twitter_monitor/twitter-worker/local_files/{user_screen_name}.csv',index=False)
+    local_dir = os.curdir
+
+
+  
+    path = Path(local_dir)
+    print(f'{path.parent.absolute()}/local_files/downloaded_csv/{user_screen_name}.csv')    
+    dataframe.to_csv(f'{path.parent.absolute()}/local_files/downloaded_csv/{user_screen_name}.csv',index=False)
